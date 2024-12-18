8808907efa6f30820a2f6fd9002d8e1e0f88701b
NicolasChambon <nico.chambon@gmail.com>
2024-12-18 12:31:00 +0100

    randomInt dunction

--- src/lesson-03.ts
+++ src/lesson-03.ts
@@ -1,1 +1,7 @@
-const nbToGuess = 43;
+const randomInt = (min: number, max: number) => {
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+};
+
+for (let i = 1; i <= 10; i++) {
+  randomInt(2, 7);
+}
