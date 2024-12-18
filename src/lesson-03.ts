1f6aeef0435521afccae0f921a5014444848280c
NicolasChambon <nico.chambon@gmail.com>
2024-12-18 15:19:28 +0100

    my proposal of 'guess a number between 1 and 100

--- src/lesson-03.ts
+++ src/lesson-03.ts
@@ -1,7 +1,39 @@
 const randomInt = (min: number, max: number) => {
-  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-for (let i = 1; i <= 10; i++) {
-  randomInt(2, 7);
+let currentProposal: number;
+let isFound = false;
+let numberOfAttemps = 0;
+let min = 0;
+let max = 100;
+
+const numberToGuess = randomInt(min, max);
+console.log('number to guess is :', numberToGuess);
+
+while (isFound === false) {
+  numberOfAttemps++;
+  currentProposal = randomInt(min, max);
+
+  if (currentProposal === numberToGuess) {
+    isFound = true;
+    console.log(`trial #${numberOfAttemps}`);
+    console.log(
+      `current proposal '${currentProposal}' is equal to number to guess '${numberToGuess}'`,
+    );
+    console.log(`number found after ${numberOfAttemps} trial(s)`);
+  } else {
+    console.log(`trial #${numberOfAttemps}`);
+    if (currentProposal > numberToGuess) {
+      max = currentProposal - 1;
+      console.log(
+        `current proposal '${currentProposal}' is greater than number to guess '${numberToGuess}'`,
+      );
+    } else {
+      min = currentProposal + 1;
+      console.log(
+        `current proposal '${currentProposal}' is less than number to guess '${numberToGuess}'`,
+      );
+    }
+  }
 }
